4398e1112711cc76704db04847cad60ebfffcc90
christophe maggiore <chrismag06000@gmail.com>
2024-10-09 10:10:13 +0200

    Initialisation du dépôt

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+		
+		int age = 30;
+		String name = "Alice";
+		boolean isStudent = false;
+		System.out.println(name + " a " + age + " ans.");
+		
+		int[] numbers = {1, 2, 3, 4, 5};
+		for (int number : numbers) {
+			System.out.println(number);
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Compteur : " + i);
+		}
+
+	}
+
+}
